19504fa69fbf8736af3f63cf25f5a43ae622c14c
Rushikesh Arote <142008272+rushiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-01-11 15:31:31 +0530

    Update FinalResume.jsx

--- client/src/ResumeFiles/FinalResume.jsx
+++ client/src/ResumeFiles/FinalResume.jsx
@@ -50,7 +50,7 @@ const FinalResume = () => {
   // Fetching Resume Data from Database
   useEffect(() => {
     axios
-      .get(`http://localhost:3001/getresumedata/${resumeId}`)
+      .get(`https://job-portal-server-orpin.vercel.app/getresumedata/${resumeId}`)
       .then((res) => {
         setResumedata(res.data);
       })
