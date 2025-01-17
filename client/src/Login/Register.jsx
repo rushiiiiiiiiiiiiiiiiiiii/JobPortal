94ff63afc860d1c182e01d1b0b5fd235351d100c
Rushikesh Arote <rushikesharote14@gmail.com>
2025-01-17 17:02:23 +0530

    hh

--- client/src/Login/Register.jsx
+++ client/src/Login/Register.jsx
@@ -27,7 +27,7 @@ const Register = () => {
     formdata.append('password', password);
 
     axios
-      .post('http://localhost:3001/reg', formdata)
+      .post('https://jobportal-server-fwl8.onrender.com/reg', formdata)
       .then((res) => {
         // console.log(res.data);
         setLoginu(true)
